e00182585c3b53fee5052d85d87ef966b16b1f12
Miguel <miguelj0916@gmail.com>
2021-11-04 19:59:14 -0500

    initial setup, install chakra, initialize custom theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Box } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import theme from "../theme";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<ChakraProvider resetCSS theme={theme}>
+				<Box bg="primary.100" w="100%" p={4} color="white">
+					Header space
+				</Box>
+				<Component {...pageProps} />
+			</ChakraProvider>
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
