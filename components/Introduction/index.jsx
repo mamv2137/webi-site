ca45172533d7f7ff901343d75d40b848c8811312
Andres <zplaguee@gmail.com>
2021-12-03 21:47:55 -0500

    roadmap structure

--- components/Introduction/index.jsx
+++ components/Introduction/index.jsx
@@ -16,14 +16,16 @@ export default function Introduction() {
         paddingInlineEnd={"0"}
         paddingInlineEnd={"0"}
         maxWidth={"100%"}
+        minWidth={"1336px"}
         display={"flex"}
         flexDirection={"column"}
       >
         <Container
+          maxWidth={"100%"}
+          minWidth={"1336px"}
           display={"flex"}
           alignItems={"center"}
           justifyContent={"space-around"}
-          maxWidth={"80%"}
         >
           <Box
             w={"35%"}
@@ -33,7 +35,7 @@ export default function Introduction() {
             alignItems={"center"}
           >
             <Image
-              boxSize="100%"
+              boxSize="350px"
               height="auto"
               src="gif.png"
               alt="Gif Webitos"
@@ -51,15 +53,22 @@ export default function Introduction() {
               Rarity of Webitos
             </Text>
             <Text padding={"8px 16px"} fontSize={"sm"}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
-              porro vero fuga quia cum quaerat nemo ipsum rem deserunt dolor,
-              facilis iste officia quis laborum aspernatur, a, esse perferendis
-              exercitationem?
+              Each Webito is completely unique but they are decorated in
+              multiple ways, each with its mischief and beauty in the Webitos
+              universe Some like it simple, after all less is more! They go
+              through life radiating good vibes, expressing freedom with their
+              style, with confidence in themselves and their only rule is to
+              have a good time.
             </Text>
           </Box>
         </Container>
-        <Container paddingBlockStart={"20"} display={"flex"} justifyContent={"center"} maxWidth={"80%"}>
-          <Box>
+        <Container
+          paddingBlockStart={"20"}
+          display={"flex"}
+          justifyContent={"space-evenly"}
+          maxWidth={"95%"}
+        >
+          <Box display={"flex"} flexDirection={"column"}>
             <Text
               padding={"8px 16px"}
               alignSelf={"start"}
@@ -68,14 +77,21 @@ export default function Introduction() {
             >
               Rarity of Webitos
             </Text>
-            <Text padding={"8px 16px"} fontSize={"sm"}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
-              porro vero fuga quia cum quaerat nemo ipsum rem deserunt dolor,
-              facilis iste officia quis laborum aspernatur, a, esse perferendis
-              exercitationem?
+            <Text
+              padding={"8px 16px"}
+              fontSize={"sm"}
+              w={"100%"}
+              maxWidth={"250px"}
+            >
+              When a Webito wants to stand out he always does it in an
+              extravagant way, you could be a rebel with piercings, tattoos or
+              accessories that will never go out of style fashion, raise catch
+              the eyes eye of other Webitos with your their o use your luego de
+              un . es decir: . catch the eye of other Webitos with your unique
+              style
             </Text>
           </Box>
-          <Box>
+          <Box display={"flex"} flexDirection={"column"}>
             <Text
               padding={"8px 16px"}
               alignSelf={"start"}
@@ -84,14 +100,18 @@ export default function Introduction() {
             >
               Rarity of Webitos
             </Text>
-            <Text padding={"8px 16px"} fontSize={"sm"}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
-              porro vero fuga quia cum quaerat nemo ipsum rem deserunt dolor,
-              facilis iste officia quis laborum aspernatur, a, esse perferendis
-              exercitationem?
+            <Text
+              padding={"8px 16px"}
+              fontSize={"sm"}
+              w={"100%"}
+              maxWidth={"250px"}
+            >
+              There are Webitos that are beyond their objectives, being great
+              executives in the Webitostock by day and savage by night where
+              modesty and taboo do not exist
             </Text>
           </Box>
-          <Box>
+          <Box display={"flex"} flexDirection={"column"}>
             <Text
               padding={"8px 16px"}
               alignSelf={"start"}
@@ -100,13 +120,43 @@ export default function Introduction() {
             >
               Rarity of Webitos
             </Text>
-            <Text padding={"8px 16px"} fontSize={"sm"}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam
-              porro vero fuga quia cum quaerat nemo ipsum rem deserunt dolor,
-              facilis iste officia quis laborum aspernatur, a, esse perferendis
-              exercitationem?
+            <Text
+              padding={"8px 16px"}
+              fontSize={"sm"}
+              w={"100%"}
+              maxWidth={"250px"}
+            >
+              The superstar Webitos are recognized by everyone in Webitosland,
+              it is impossible not to notice when one of them comes out, they
+              are the only Webitos that symbolize the most exotic in their own
+              careers, they are seen as Angels or Demons, Rockstar Spies, Nature
+              Fighters or great disco dancers
             </Text>
           </Box>
+        </Container>
+        <Container
+          paddingBlockStart={"20"}
+          display={"flex"}
+          flexDirection={"column"}
+          justifyContent={"space-evenly"}
+          maxWidth={"95%"}
+        >
+          <Text padding={"8px 16px"} fontSize={"2xl"} w={"100%"}>
+            10% - A little bit of joy for the co-founders, a.k.a to stop eating
+            instant ramen noodles for a month at least
+          </Text>
+          <Text padding={"8px 16px"} fontSize={"2xl"} w={"100%"}>
+            10% - A little bit of joy for the co-founders, a.k.a to stop eating
+            instant ramen noodles for a month at least
+          </Text>
+          <Text padding={"8px 16px"} fontSize={"2xl"} w={"100%"}>
+            10% - A little bit of joy for the co-founders, a.k.a to stop eating
+            instant ramen noodles for a month at least
+          </Text>
+          <Text padding={"8px 16px"} fontSize={"2xl"} w={"100%"}>
+            10% - A little bit of joy for the co-founders, a.k.a to stop eating
+            instant ramen noodles for a month at least
+          </Text>
         </Container>
       </Container>
     </>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
-import { Box } from "@chakra-ui/layout";
 import Introduction from "../components/Introduction";
 import { Container } from '@chakra-ui/react'
 
 export default function Home(){
   return(
-    <Container h="100vh" display="flex" alignItems="center" justifyContent="center">
+    <Container display="flex" alignItems="center" justifyContent="center">
         <Introduction />
     </Container>
   );
