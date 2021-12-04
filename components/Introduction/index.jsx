befe1ffd2166e8571c61d02e4cb575c10aab53d6
Andres <zplaguee@gmail.com>
2021-12-03 22:17:03 -0500

    testing bg animation

--- components/Introduction/index.jsx
+++ components/Introduction/index.jsx
@@ -10,7 +10,12 @@ import {
 
 export default function Introduction() {
   return (
-    <>
+    <><div class='ripple-background'>
+        <div class='circle xxlarge shade1'></div>
+        <div class='circle xlarge shade2'></div>
+        <div class='circle large shade3'></div>
+        <div class='circle mediun shade4'></div>
+         <div class='circle small shade5'></div>
       <Container
         fontFamily={"Raleway"}
         paddingInlineEnd={"0"}
@@ -159,6 +164,7 @@ export default function Introduction() {
           </Text>
         </Container>
       </Container>
+      </div>
     </>
   );
 }
